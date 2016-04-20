ebfaa97c435352c981f8089375a0cd37843ab64f
Sam Briggs <wasso14@hotmail.com>
2016-04-20 22:03:00 +0100

    WORK: Made BaseViewModel abstract

--- TemplateBuilder/ViewModel/BaseViewModel.cs
+++ TemplateBuilder/ViewModel/BaseViewModel.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace TemplateBuilder.ViewModel
 {
-    public class BaseViewModel : INotifyPropertyChanged
+    public abstract class BaseViewModel : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
 
