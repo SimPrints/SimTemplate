4ed7390d773c1a6eace7cb1b16c2489ff73365e7
Sam Briggs <wasso14@hotmail.com>
2016-10-02 21:06:41 +0100

    Added OpenSource copywrite header to source

--- SimTemplate/Utilities/IWindowService.cs
+++ SimTemplate/Utilities/IWindowService.cs
@@ -1,4 +1,20 @@
-﻿using SimTemplate.ViewModels;
+﻿// Copyright 2016 Sam Briggs
+//
+// This file is part of SimTemplate.
+//
+// SimTemplate is free software: you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the Free Software 
+// Foundation, either version 3 of the License, or (at your option) any later
+// version.
+//
+// SimTemplate is distributed in the hope that it will be useful, but WITHOUT ANY 
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR 
+// A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along with
+// SimTemplate. If not, see http://www.gnu.org/licenses/.
+//
+using SimTemplate.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
