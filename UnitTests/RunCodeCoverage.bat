4906f853e94f2c1a823dc9fb4bc096337cdfd3d8
Sam Briggs <wasso14@hotmail.com>
2016-10-18 08:35:54 +0100

    #8: Updated RunCodeCoverage.bat to provide a concise summary for Github.

--- UnitTests/RunCodeCoverage.bat
+++ UnitTests/RunCodeCoverage.bat
@@ -1,14 +1,19 @@
 ﻿@echo OFF
+
+echo Creating a 'Results' folder if it does not exist...
+if not exist "%~dp0TestResults" mkdir "%~dp0TestResults"
+
 echo Creating a 'GeneratedReports' folder if it does not exist...
 if not exist "%~dp0GeneratedReports" mkdir "%~dp0GeneratedReports"
  
 echo Removing any previous test execution files to prevent issues overwriting...
-IF EXIST "%~dp0AutomatedSimTemplateTests.trx" del "%~dp0AutomatedSimTemplateTests.trx%"
+IF EXIST "%~dp0TestResults\AutomatedSimTemplateTests.trx" del "%~dp0TestResults\AutomatedSimTemplateTests.trx%"
  
 echo Removing any previously created test output directories...
-CD %~dp0
+CD %~dp0\TestResults
 FOR /D /R %%X IN (%USERNAME%*) DO RD /S /Q "%%X"
- 
+CD %~dp0
+
 echo Running the tests against the targeted output...
 call :RunOpenCoverUnitTestMetrics
  
@@ -27,16 +32,17 @@ exit /b %errorlevel%
 "%~dp0..\packages\OpenCover.4.6.519\tools\OpenCover.Console.exe" ^
 -register:user ^
 -target:"%VS140COMNTOOLS%\..\IDE\mstest.exe" ^
--targetargs:"/testcontainer:\"%~dp0AutomatedSimTemplateTests\bin\Debug\AutomatedSimTemplateTests.dll\" /resultsfile:\"%~dp0AutomatedSimTemplateTests.trx\"" ^
+-targetargs:"/testcontainer:\"%~dp0AutomatedSimTemplateTests\bin\Debug\AutomatedSimTemplateTests.dll\" /resultsfile:\"%~dp0TestResults\AutomatedSimTemplateTests.trx\"" ^
 -filter:"+[SimTemplate*]* -[SimTemplate.*Tests]* -[*]SimTemplate.RouteConfig" ^
 -mergebyhash ^
 -skipautoprops ^
 -output:"%~dp0GeneratedReports\AutomatedSimTemplateTests.xml"
 exit /b %errorlevel%
- 
+
 :RunReportGeneratorOutput
 "%~dp0..\packages\ReportGenerator.2.4.5.0\tools\ReportGenerator.exe" ^
 -reports:"%~dp0GeneratedReports\AutomatedSimTemplateTests.xml" ^
+-reporttypes:"Html;HtmlSummary" ^
 -targetdir:"%~dp0GeneratedReports\ReportGenerator Output"
 exit /b %errorlevel%
  
